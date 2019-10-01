46ce8e9cb5085fe3b30611648155149f24f59a63
Angelo Acosta <angelocastellanos99@gmail.com>
2019-09-30 22:02:02 -0500

    PC2 AXEL CHAVEZ

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Agent : SBAgent
+{
+	public Transform target;
+	public float radio;
+	public Vector3 center;
+	public Transform centroObjeto;
+
+	void Start()
+	{		
+		
+		maxSpeed = 10f;
+		maxSteer = 0.5f;
+		radio= 3;
+		center = centroObjeto.transform.position;
+		
+	}
+
+	void Update()
+	{
+		velocity += SteeringBehaviours.Seek(this, target);
+		velocity += SteeringBehaviours.InsideCircle(this, center , radio);
+		transform.position += velocity*Time.deltaTime;
+	}
+
+
+		void OnDrawGizmos()
+	{
+		Gizmos.color= Color.red;
+		Gizmos.DrawWireSphere(center,radio);
+		
+
+	}
+}
---
+++ Assets/Scripts/SteeringBehaviours.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+
+public class SteeringBehaviours
+{
+    static public Vector3 Seek(SBAgent agent, Transform target)
+    {
+        // cálculo del vector deseado
+        Vector3 desired = (target.position - agent.transform.position).normalized * agent.maxSpeed;
+
+        // cálculo de los demás vectores
+        Vector3 steer = Vector3.ClampMagnitude(desired - agent.velocity, agent.maxSteer);
+
+        return steer;
+    }
+
+    static public Vector3 Flee(SBAgent agent, Transform target)
+    {
+        // cálculo del vector deseado
+        Vector3 desired = -(target.position - agent.transform.position).normalized * agent.maxSpeed;
+
+        // cálculo de los demás vectores
+        Vector3 steer = Vector3.ClampMagnitude(desired - agent.velocity, agent.maxSteer);
+
+        return steer;
+    }
+
+    static public Vector3 Arrive(SBAgent agent, Transform target, float range)
+    {
+        // cálculo del vector deseado
+        Vector3 desired;
+        Vector3 difference = (target.position - agent.transform.position);
+        float distance = difference.magnitude;
+
+        desired = difference.normalized * agent.maxSpeed;
+
+        if (distance < range)
+        {
+            desired *= distance / range;
+        }
+
+        // cálculo de los demás vectores
+        Vector3 steer = Vector3.ClampMagnitude(desired - agent.velocity, agent.maxSteer);
+
+        return steer;
+    }
+
+
+
+    static public Vector3 InsideCircle(SBAgent agent, Vector3 center, float radio)
+    {
+        // cálculo del vector deseado
+        Vector3 desired;
+        Vector3 steer;
+
+
+        float distance = (agent.transform.position - center).magnitude;
+
+
+
+        if (distance > radio)
+        {
+            desired = (center - agent.transform.position).normalized * agent.maxSpeed;
+
+            //desired *= radio / distance;
+        }
+        else
+        {
+            desired = Vector3.zero;
+
+        }
+
+        // cálculo de los demás vectores
+
+        steer = Vector3.ClampMagnitude(desired - agent.velocity, agent.maxSteer);
+        return steer;
+    }
+
+
+
+
+}
